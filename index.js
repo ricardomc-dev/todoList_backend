bd740f625e3b51a96b6223c7c67924cfaefe059f
Ricardo Carvalho <ricardomc.dev@gmail.com>
2022-07-04 17:23:21 -0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.get('/', (_req, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
